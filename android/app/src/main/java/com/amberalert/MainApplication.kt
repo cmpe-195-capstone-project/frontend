13b686e51f13be5a63d97cfcbbe001686454efef
carlosq-mv <cquiroz7869@gmail.com>
2025-11-15 15:30:35 -0800

    feat(native-code): added new foreground service

--- android/app/src/main/java/com/amberalert/MainApplication.kt
+++ android/app/src/main/java/com/amberalert/MainApplication.kt
@@ -19,7 +19,9 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          return PackageList(this).packages
+          val packages = PackageList(this).packages.toMutableList()
+          packages.add(ForegroundServicePackage())
+          return packages
         }
 
         override fun getJSMainModuleName(): String = "index"
