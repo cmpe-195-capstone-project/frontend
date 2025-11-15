d61576a85682c11e8e9443c5dd29626828ca9b2e
carlosq-mv <cquiroz7869@gmail.com>
2025-11-15 15:36:03 -0800

    feat(app): add context into application

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './screens/HomeScreen';
 import FirePrepScreen from './screens/FirePrepScreen';
 import AlertsScreen from './screens/AlertsScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import {LocationProvider} from './context/LocationContext';
+import {AppInitProvider} from './context/AppInitContext';
 
 export type RootTabParamList = {
   'Fire Prep': undefined;
@@ -14,17 +16,22 @@ export type RootTabParamList = {
   Settings: undefined;
 };
 
-const Tab = createBottomTabNavigator<RootTabParamList>();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Fire Prep" component={FirePrepScreen} />
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Alerts" component={AlertsScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <LocationProvider>
+      <AppInitProvider>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={{headerShown: false}}>
+            <Tab.Screen name="Fire Prep" component={FirePrepScreen} />
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Alerts" component={AlertsScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </AppInitProvider>
+    </LocationProvider>
   );
 }
