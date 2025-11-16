19f093560cbda238377b95afc2ca39699bd62e3a
carlosq-mv <cquiroz7869@gmail.com>
2025-11-16 01:33:04 -0800

    feat(location): remove permission and add method for watching location changes

--- context/LocationContext.js
+++ context/LocationContext.js
@@ -1,11 +1,11 @@
-import React, {createContext, useState, useEffect} from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import Geolocation from 'react-native-geolocation-service';
-import {PermissionsAndroid, Platform} from 'react-native';
+import { NativeModules } from 'react-native';
 
 export const LocationContext = createContext({
   location: FALLBACK,
   status: "",
-  refresh: () => {}
+  refresh: () => { }
 });
 
 export const FALLBACK = {
@@ -16,33 +16,74 @@ export const FALLBACK = {
 };
 
 export function LocationProvider({ children }) {
-
   const [location, setLocation] = useState(FALLBACK);
   const [status, setStatus] = useState('Booting...');
   const [loading, setLoading] = useState(true);
+  const [watchId, setWatchId] = useState(null);
+
 
   useEffect(() => {
-    console.log('[Location] Context initializing...');
-    getLocation();
+    // if (!ready) return;
+    (async () => {
+
+      await getLocation();
+      await watchLocation();
+    })();
+
+    return () => {
+      if (watchId !== null) {
+        console.log("[Location] Clearing location watcher");
+        Geolocation.clearWatch(watchId);
+      }
+    }
   }, []);
+
+
+  const watchLocation = async () => {
+    console.log('[WatchLocation] Starting to watch location');
+
+    const id = Geolocation.watchPosition(
+      async pos => {
+        const { latitude, longitude } = pos.coords;
+
+        const next = {
+          latitude,
+          longitude,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        };
+
+        // Update UI location
+        setLocation(next);
+
+        console.log(`[WatchLocation] New updated location: latitude[${next.latitude}] longitude[${next.longitude}]`)
+
+        // Send update to native
+        NativeModules.ForegroundServiceModule.sendUpdateLocation(
+          next.latitude.toString(),
+          next.longitude.toString()
+        );
+      },
+      err => {
+        console.log('[WatchLocation] WATCHING GPS SERVICE ERROR:', err.code, err.message);
+      },
+      {
+        enableHighAccuracy: true,
+        distanceFilter: 10,   // triggers after user moves 10 meters
+        interval: 10000,      // Android: 10 seconds max between events
+        fastestInterval: 5000, // Android: at least 5 seconds apart
+        accuracy: { android: "balanced" }
+      }
+
+    );
+
+    setWatchId(id);
+  }
 
   // Get location on app mount
   const getLocation = async () => {
     console.log('[Location] Booting location service...');
     console.log('[Location] starting permission flow');
-
-    if (Platform.OS === 'android') {
-      const res = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      );
-      console.log('[Location] permission result:', res);
-
-      if (res !== PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('[Location] Permision denied');
-        setStatus('Location permission denied. Showing default area.');
-        return;
-      }
-    }
 
     console.log('[Location] Fetching GPS...');
 
@@ -51,7 +92,7 @@ export function LocationProvider({ children }) {
       async pos => {
         console.log('[Location] GPS OK:', JSON.stringify(pos));
 
-        const {latitude, longitude} = pos.coords;
+        const { latitude, longitude } = pos.coords;
         const next = {
           latitude,
           longitude,
@@ -61,9 +102,13 @@ export function LocationProvider({ children }) {
 
         setLocation(next);
         setStatus('');
-        console.log("the location i ahve" + next);
-        // await storeData('location', JSON.stringify(next));
-        // await AsyncStorage.setItem("location", JSON.stringify(next));
+        console.log("the location I have" + next);
+
+        NativeModules.ForegroundServiceModule.sendUpdateLocation(
+          next.latitude.toString(),
+          next.longitude.toString()
+        );
+
         setLoading(false);
       },
       err => {
@@ -71,9 +116,9 @@ export function LocationProvider({ children }) {
         setStatus(`GPS error (${err.code}). Showing default area.`);
       },
       {
-        enableHighAccuracy: true, 
-        timeout: 10000, 
-        maximumAge: 5000, 
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 5000,
         accuracy: { android: "balanced" }
       },
     );
